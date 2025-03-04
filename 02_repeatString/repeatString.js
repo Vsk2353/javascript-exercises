5593d0752be9d71c8379715ae2a56e9cf3278d52
Visky <65082238+Vsk2353@users.noreply.github.com>
2025-03-04 13:42:21 +0530

    Implement repeatString function to repeat a given string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    if(num===0){
+        string='';
+    }
 
+    if(num<0){
+        string='ERROR';
+    }
+
+    let adder = string;
+    for (i=1;i<num;i++){
+        string+=adder;
+    }
+    return string;
 };
 
 // Do not edit below this line
