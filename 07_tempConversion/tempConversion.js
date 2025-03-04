96dd534e560a439b76f8a4c930759143385d6b51
Visky <65082238+Vsk2353@users.noreply.github.com>
2025-03-04 15:32:59 +0530

    Implement function to convert temperatures

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let ans1 = 0;
+  ans1 = (temp-32)*(5/9);
+  return Math.round((ans1*10))/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let ans2 = 0;
+  ans2 = temp*(9/5) + 32;
+  return Math.round((ans2*10))/10;
 };
 
 // Do not edit below this line
