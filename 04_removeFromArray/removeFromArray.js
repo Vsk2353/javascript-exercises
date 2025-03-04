ca479494453b335cc95d00d22b1219c777478d14
Visky <65082238+Vsk2353@users.noreply.github.com>
2025-03-04 14:49:36 +0530

    Implement function to remove multiple elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(a, ...b) {
+    let array=a.filter(check);
+
+    function check(item){
+        return !(b.includes(item));
+    }
+    return array;
 };
 
 // Do not edit below this line
