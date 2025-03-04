754388151b0085be1bc1824b45f401023cb50ed3
Visky <65082238+Vsk2353@users.noreply.github.com>
2025-03-04 15:05:42 +0530

    Implement function to calculate sum within a given range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum=0;
+
+    if(!Number.isInteger(a) || !Number.isInteger(b)){
+        return("ERROR");
+    }    
+    if(a<0 || b<0){
+        return("ERROR");
+    }
+
+
+
+    if(a<b){
+        for(i=a;i<=b;i++){
+            sum=sum+i;
+        }
+        return sum;
+    }
+    else{
+        for(i=b;i<=a;i++){
+            sum=sum+i;
+        }
+        return sum;
+    }
 
 };
 
